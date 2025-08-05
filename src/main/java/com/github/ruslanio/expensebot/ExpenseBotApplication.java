8bc0cf440b5e40e4c01660504493923c7764ea09
rupavlov <rupavlov@devcats.kg>
2025-08-05 23:42:40 +0600

    initial commit

---
+++ src/main/java/com/github/ruslanio/expensebot/ExpenseBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.ruslanio.expensebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ruslanio/expensebot/ExpenseBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ruslanio.expensebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
